3ae762a30919d1cc673c26505713a0adda86f2e5
Seun Olubajo <olubajoseun@yahoo.com>
2019-09-11 03:45:10 +0100

    3rd Drop(3)

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -18,7 +18,7 @@ namespace WishList
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
-            services.AddDbContext<ApplicationDbContext>(options =>options.UseInMemoryDatabase("WishList"));
+            services.AddDbContext<ApplicationDbContext>(options => options.UseInMemoryDatabase("WishList"));
 
         }
 
